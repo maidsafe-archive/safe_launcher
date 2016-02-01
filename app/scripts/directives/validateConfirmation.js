247a43e49de72a1edda9e20868baf5751895fb40
shankar2105 <shankar21mail@gmail.com>
2016-02-01 20:57:24 +0530

    Confirmation field validation integrated

---
+++ app/scripts/directives/validateConfirmation.js
@@ -0,0 +1,24 @@
+/**
+ * Validate Confirmation
+ */
+window.safeLauncher.directive('mslValidateConfirmation', function() {
+  var link = function(scope, element, attrs, controller) {
+    element.bind('blur', function() {
+      var value = element.val();
+      var inpName = angular.element(element[0]).attr('name');
+      var formName = element[0].form.name;
+      var form = scope[formName][inpName];
+      form.$setValidity('customValidation', false);
+      var targetName = attrs.targetName;
+      var targetValue = scope[formName][targetName].$viewValue;
+      if (value !== targetValue) {
+        return;
+      }
+      form.$setValidity('customValidation', true);
+    });
+  };
+  return {
+    restrict: 'A',
+    link: link
+  };
+});
