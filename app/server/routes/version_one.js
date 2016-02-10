938a033eb2c9703563ad50a93f29380ff047c73f
krishna <krishnaof1988@gmail.com>
2016-02-10 19:03:19 +0530

    comments in router

--- app/server/routes/version_one.js
+++ app/server/routes/version_one.js
@@ -5,10 +5,12 @@ import * as Auth from '../controllers/auth';
 var router = express.Router();
 router.post('/auth', Auth.authorise);
 router.delete('/auth', Auth.revoke);
+// NFS - DIRECTORY API
 router.post('/nfs/directory', NFS.createDirectory);
 router.get('/nfs/directory/:dirPath/:isPathShared', NFS.getDirectory);
 router.delete('/nfs/directory/:dirPath/:isPathShared', NFS.deleteDirectory);
 router.put('/nfs/directory/:dirPath/:isPathShared', NFS.modifyDirectory);
+// NFS - FILE API
 router.post('/nfs/file', NFS.createFile);
 router.delete('/nfs/file/:filePath/:isPathShared', NFS.deleteFile);
 router.put('/nfs/file/metadata/:filePath/:isPathShared', NFS.modifyFileMeta);
