4be497c4855e793bead0deb4ae3b92020a4e4650
shankar2105 <shankar21mail@gmail.com>
2016-02-11 17:31:41 +0530

    IsPathShared variable changed to optional params

--- app/server/controllers/dns.js
+++ app/server/controllers/dns.js
@@ -1,7 +1,7 @@
 import sessionManager from '../session_manager';
 import { formatResponse, ResponseHandler } from '../utils';
 
-export var register = function(req, res) {
+var registerOrAddService = function(req, res, isRegister) {
   let sessionInfo = sessionManager.get(req.headers.sessionId);
   let reqBody = JSON.parse(req.body.toString());
   if (!reqBody.longName) {
@@ -13,15 +13,21 @@ export var register = function(req, res) {
   if (!reqBody.serviceHomeDirPath) {
     return res.status(400).send('Invalid request. serviceHomeDirPath missing');
   }
-  try {
-    reqBody.isPathShared = JSON.parse(reqBody.isPathShared);
-  } catch (e) {
-    res.status(500).send(e.toString())
-  }
-  if (!reqBody.hasOwnProperty('isPathShared') || !(typeof reqBody.isPathShared === 'boolean')) {
-    return res.status(400).send('Invalid request. isPathShared missing');
-  }
+  reqBody.isPathShared = reqBody.isPathShared || false;
   let responseHandler = new ResponseHandler(res, sessionInfo);
-  req.app.get('api').dns.register(reqBody.longName, reqBody.serviceName, reqBody.serviceHomeDirPath,
-    reqBody.isPathShared, sessionInfo.hasSafeDriveAccess(), sessionInfo.appDirKey, responseHandler.onResponse);
+  if (isRegister) {
+    req.app.get('api').dns.register(reqBody.longName, reqBody.serviceName, reqBody.serviceHomeDirPath,
+      reqBody.isPathShared, sessionInfo.hasSafeDriveAccess(), sessionInfo.appDirKey, responseHandler.onResponse);
+  } else {
+    req.app.get('api').dns.addService(reqBody.longName, reqBody.serviceName, reqBody.serviceHomeDirPath,
+      reqBody.isPathShared, sessionInfo.hasSafeDriveAccess(), sessionInfo.appDirKey, responseHandler.onResponse);
+  }
+};
+
+export var register = function(req, res) {
+  registerOrAddService(req, res, true);
+};
+
+export var addService = function(req, res) {
+  registerOrAddService(req, res, false);
 };
--- app/server/routes/version_one.js
+++ app/server/routes/version_one.js
@@ -8,15 +8,15 @@ router.post('/auth', Auth.authorise);
 router.delete('/auth', Auth.revoke);
 // NFS - DIRECTORY API
 router.post('/nfs/directory', NFS.createDirectory);
-router.get('/nfs/directory/:dirPath/:isPathShared', NFS.getDirectory);
-router.delete('/nfs/directory/:dirPath/:isPathShared', NFS.deleteDirectory);
-router.put('/nfs/directory/:dirPath/:isPathShared', NFS.modifyDirectory);
+router.get('/nfs/directory/:dirPath/:isPathShared?', NFS.getDirectory);
+router.delete('/nfs/directory/:dirPath/:isPathShared?', NFS.deleteDirectory);
+router.put('/nfs/directory/:dirPath/:isPathShared?', NFS.modifyDirectory);
 // NFS - FILE API
 router.post('/nfs/file', NFS.createFile);
-router.delete('/nfs/file/:filePath/:isPathShared', NFS.deleteFile);
-router.put('/nfs/file/metadata/:filePath/:isPathShared', NFS.modifyFileMeta);
-router.put('/nfs/file/:filePath/:isPathShared', NFS.modifyFileContent);
-router.get('/nfs/file/:filePath/:isPathShared', NFS.getFile);
+router.delete('/nfs/file/:filePath/:isPathShared?', NFS.deleteFile);
+router.put('/nfs/file/metadata/:filePath/:isPathShared?', NFS.modifyFileMeta);
+router.put('/nfs/file/:filePath/:isPathShared?', NFS.modifyFileContent);
+router.get('/nfs/file/:filePath/:isPathShared?', NFS.getFile);
 // DNS API
 router.post('/dns/register', DNS.register);
 export { router as versionOneRouter };
