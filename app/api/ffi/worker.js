a27dd993e72af2882f09a22ce8bea3b969a04c41
krishna <krishnaof1988@gmail.com>
2016-05-17 17:53:11 +0530

    1. FFI of safe_core used instead of safe_ffi 2. Readme updated 3. Version update

--- app/api/ffi/worker.js
+++ app/api/ffi/worker.js
@@ -5,7 +5,7 @@ var rootFolder = __dirname;
 if (rootFolder.indexOf('app.asar') > 0) {
   rootFolder = path.resolve(rootFolder, '../../../', 'app.asar.unpacked/api/ffi/');
 }
-var libPath = path.resolve(rootFolder, (process.platform === 'win32' ? 'safe_ffi' : 'libsafe_ffi'));
+var libPath = path.resolve(rootFolder, (process.platform === 'win32' ? 'safe_core' : 'libsafe_core'));
 var hanlder = new Hanlder(libPath);
 
 var onMessage = hanlder.dispatcher;
