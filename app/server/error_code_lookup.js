853c59a93bac62fcf568b489d744d7d9242814f7
krishna <krishnaof1988@gmail.com>
2016-05-26 20:40:51 +0530

    Error codes updated

--- app/server/error_code_lookup.js
+++ app/server/error_code_lookup.js
@@ -16,32 +16,52 @@ export function errorCodeLookup(errCode) {
       return 'CoreError::ReceivedUnexpectedData';
     case CLIENT_ERROR_START_RANGE - 5:
       return 'CoreError::VersionCacheMiss';
-    case CLIENT_ERROR_START_RANGE - 7:
+    case CLIENT_ERROR_START_RANGE - 6:
       return 'CoreError::RootDirectoryAlreadyExists';
-    case CLIENT_ERROR_START_RANGE - 8:
+    case CLIENT_ERROR_START_RANGE - 7:
       return 'CoreError::RandomDataGenerationFailure';
-    case CLIENT_ERROR_START_RANGE - 9:
+    case CLIENT_ERROR_START_RANGE - 8:
       return 'CoreError::OperationForbiddenForClient';
-    case CLIENT_ERROR_START_RANGE - 10:
+    case CLIENT_ERROR_START_RANGE - 9:
       return 'CoreError::Unexpected';
-    case CLIENT_ERROR_START_RANGE - 11:
+    case CLIENT_ERROR_START_RANGE - 10:
       return 'CoreError::RoutingError';
-    case CLIENT_ERROR_START_RANGE - 12:
+    case CLIENT_ERROR_START_RANGE - 11:
       return 'CoreError::RoutingInterfaceError';
-    case CLIENT_ERROR_START_RANGE - 13:
+    case CLIENT_ERROR_START_RANGE - 12:
       return 'CoreError::UnsupportedSaltSizeForPwHash';
-    case CLIENT_ERROR_START_RANGE - 14:
+    case CLIENT_ERROR_START_RANGE - 13:
       return 'CoreError::UnsuccessfulPwHash';
-    case CLIENT_ERROR_START_RANGE - 15:
+    case CLIENT_ERROR_START_RANGE - 14:
       return 'CoreError::OperationAborted';
-    case CLIENT_ERROR_START_RANGE - 16:
+    case CLIENT_ERROR_START_RANGE - 15:
       return 'CoreError::MpidMessagingError';
+    case CLIENT_ERROR_START_RANGE - 16:
+      return 'CoreError::GetFailure::GetError::NoSuchAccount';
     case CLIENT_ERROR_START_RANGE - 17:
-      return 'CoreError::GetFailure';
+      return 'CoreError::GetFailure::GetError::NoSuchData';
     case CLIENT_ERROR_START_RANGE - 18:
-      return 'CoreError::MutationFailure';
+      return 'CoreError::GetFailure::GetError::NetworkOther';
     case CLIENT_ERROR_START_RANGE - 19:
-      return 'CoreError::SelfEncryptionError';    
+      return 'CoreError::MutationFailure::MutationError::NoSuchAccount';
+    case CLIENT_ERROR_START_RANGE - 20:
+      return 'CoreError::MutationFailure::MutationError::AccountExists';
+    case CLIENT_ERROR_START_RANGE - 21:
+      return 'CoreError::MutationFailure::MutationError::NoSuchData';
+    case CLIENT_ERROR_START_RANGE - 22:
+      return 'CoreError::MutationFailure::MutationError::DataExists';
+    case CLIENT_ERROR_START_RANGE - 23:
+      return 'CoreError::MutationFailure::MutationError::LowBalance';
+    case CLIENT_ERROR_START_RANGE - 24:
+      return 'CoreError::MutationFailure::MutationError::InvalidSuccessor';
+    case CLIENT_ERROR_START_RANGE - 25:
+      return 'CoreError::MutationFailure::MutationError::InvalidOperation';
+    case CLIENT_ERROR_START_RANGE - 26:
+      return 'CoreError::MutationFailure::MutationError::NetworkOther';
+    case CLIENT_ERROR_START_RANGE - 27:
+      return 'CoreError::MutationFailure::MutationError::NetworkFull';
+    case CLIENT_ERROR_START_RANGE - 28:
+      return 'CoreError::SelfEncryptionError';
     case NFS_ERROR_START_RANGE - 1:
       return 'NfsError::DirectoryAlreadyExistsWithSameName';
     case NFS_ERROR_START_RANGE - 2:
