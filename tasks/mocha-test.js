49156e101a08e84c0e7e3d57884876c7601f1a68
shankar2105 <shankar21mail@gmail.com>
2016-06-01 12:37:52 +0530

    travis updated

--- tasks/mocha-test.js
+++ tasks/mocha-test.js
@@ -11,8 +11,7 @@ var babel = require('gulp-babel');
 var fse = require('fs-extra')
 var path = require('path');
 var os = require('os');
-// var exec = require('child_process').exec;
-var childProcess = require('child_process');
+var exec = require('child_process').exec;
 
 var destDir = path.resolve('testApp');
 var ffiName = null;
@@ -22,7 +21,7 @@ if (os.platform() === 'darwin') {
 } else if(os.platform() === 'linux') {
   ffiName = 'libsafe_core.so';
 } else {
-  ffiName = 'safe_ffi.dll';
+  ffiName = 'safe_core.dll';
 }
 var apiPaths = [
   './app/api/**',
@@ -82,7 +81,7 @@ gulp.task('copy', function() {
 });
 
 gulp.task('installPackages', function(cb) {
-  childProcess.exec('cd testApp && npm install && cd .. && gulp msvc_rebuild --env=test', function(err, stdout, stderr) {
+  exec('cd testApp && npm install && cd .. && gulp msvc_rebuild --env=test', function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
@@ -106,11 +105,11 @@ var executeTest = function(cb) {
   // .pipe(stylish.combineWithHintResults()) // combine with jshint results
   // .pipe(jshint.reporter('jshint-stylish'));
   // runMochaTests(cb);
-    childProcess.exec(gulpPath + ' --renderder --compilers js:babel-core/register --timeout 50000 -R mocha-unfunk-reporter ./tests', function(err, stdout, stderr) {
-      console.log(stdout);
-      console.log(stderr);
-      cb(err);
-    });
+  exec(gulpPath + ' --renderder --compilers js:babel-core/register --timeout 50000 -R mocha-unfunk-reporter ./tests', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 };
 
 gulp.task('test', [ 'clean', 'babelApi', 'babelServer', 'copy', 'installPackages' ], executeTest);
