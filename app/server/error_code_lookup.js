6bb9749d25fc1f33cd4ca45619ca974323755035
shankar2105 <shankar21mail@gmail.com>
2016-06-22 13:40:12 +0530

    feat/grep_error_code.js: Grep error code from safe_core and write it into error_code_lookup.js file
    Grep Core, NFS and DNS error code from safe_core code and write it into app/server/error_code_lookup.js

--- app/server/error_code_lookup.js
+++ app/server/error_code_lookup.js
@@ -1,122 +1,103 @@
 export function errorCodeLookup(errCode) {
-  let CLIENT_ERROR_START_RANGE = -1;
-  let NFS_ERROR_START_RANGE = CLIENT_ERROR_START_RANGE - 500;
-  let DNS_ERROR_START_RANGE = NFS_ERROR_START_RANGE - 500;
-  let FFI_ERROR_START_RANGE = DNS_ERROR_START_RANGE - 500;
-  switch (errCode) {
-    case CLIENT_ERROR_START_RANGE:
-      return 'CoreError::StructuredDataHeaderSizeProhibitive';
-    case CLIENT_ERROR_START_RANGE - 1:
-      return 'CoreError::UnsuccessfulEncodeDecode';
-    case CLIENT_ERROR_START_RANGE - 2:
-      return 'CoreError::AsymmetricDecipherFailure';
-    case CLIENT_ERROR_START_RANGE - 3:
-      return 'CoreError::SymmetricDecipherFailure';
-    case CLIENT_ERROR_START_RANGE - 4:
-      return 'CoreError::ReceivedUnexpectedData';
-    case CLIENT_ERROR_START_RANGE - 5:
-      return 'CoreError::VersionCacheMiss';
-    case CLIENT_ERROR_START_RANGE - 6:
-      return 'CoreError::RootDirectoryAlreadyExists';
-    case CLIENT_ERROR_START_RANGE - 7:
-      return 'CoreError::RandomDataGenerationFailure';
-    case CLIENT_ERROR_START_RANGE - 8:
-      return 'CoreError::OperationForbiddenForClient';
-    case CLIENT_ERROR_START_RANGE - 9:
-      return 'CoreError::Unexpected';
-    case CLIENT_ERROR_START_RANGE - 10:
-      return 'CoreError::RoutingError';
-    case CLIENT_ERROR_START_RANGE - 11:
-      return 'CoreError::RoutingInterfaceError';
-    case CLIENT_ERROR_START_RANGE - 12:
-      return 'CoreError::UnsupportedSaltSizeForPwHash';
-    case CLIENT_ERROR_START_RANGE - 13:
-      return 'CoreError::UnsuccessfulPwHash';
-    case CLIENT_ERROR_START_RANGE - 14:
-      return 'CoreError::OperationAborted';
-    case CLIENT_ERROR_START_RANGE - 15:
-      return 'CoreError::MpidMessagingError';
-    case CLIENT_ERROR_START_RANGE - 16:
-      return 'CoreError::GetFailure::GetError::NoSuchAccount';
-    case CLIENT_ERROR_START_RANGE - 17:
-      return 'CoreError::GetFailure::GetError::NoSuchData';
-    case CLIENT_ERROR_START_RANGE - 18:
-      return 'CoreError::GetFailure::GetError::NetworkOther';
-    case CLIENT_ERROR_START_RANGE - 19:
-      return 'CoreError::MutationFailure::MutationError::NoSuchAccount';
-    case CLIENT_ERROR_START_RANGE - 20:
-      return 'CoreError::MutationFailure::MutationError::AccountExists';
-    case CLIENT_ERROR_START_RANGE - 21:
-      return 'CoreError::MutationFailure::MutationError::NoSuchData';
-    case CLIENT_ERROR_START_RANGE - 22:
-      return 'CoreError::MutationFailure::MutationError::DataExists';
-    case CLIENT_ERROR_START_RANGE - 23:
-      return 'CoreError::MutationFailure::MutationError::LowBalance';
-    case CLIENT_ERROR_START_RANGE - 24:
-      return 'CoreError::MutationFailure::MutationError::InvalidSuccessor';
-    case CLIENT_ERROR_START_RANGE - 25:
-      return 'CoreError::MutationFailure::MutationError::InvalidOperation';
-    case CLIENT_ERROR_START_RANGE - 26:
-      return 'CoreError::MutationFailure::MutationError::NetworkOther';
-    case CLIENT_ERROR_START_RANGE - 27:
-      return 'CoreError::MutationFailure::MutationError::NetworkFull';
-    case CLIENT_ERROR_START_RANGE - 28:
-      return 'CoreError::SelfEncryptionError';
-    case NFS_ERROR_START_RANGE - 1:
-      return 'NfsError::DirectoryAlreadyExistsWithSameName';
-    case NFS_ERROR_START_RANGE - 2:
-      return 'NfsError::DestinationAndSourceAreSame';
-    case NFS_ERROR_START_RANGE - 3:
-      return 'NfsError::DirectoryNotFound';
-    case NFS_ERROR_START_RANGE - 4:
-      return 'NfsError::FileAlreadyExistsWithSameName';
-    case NFS_ERROR_START_RANGE - 5:
-      return 'NfsError::FileDoesNotMatch';
-    case NFS_ERROR_START_RANGE - 6:
-      return 'NfsError::FileNotFound';
-    case NFS_ERROR_START_RANGE - 7:
-      return 'NfsError::InvalidRangeSpecified';
-    case NFS_ERROR_START_RANGE - 8:
-      return 'NfsError::ParameterIsNotValid';
-    case NFS_ERROR_START_RANGE - 9:
-      return 'NfsError::UnexpectedError';
-    case NFS_ERROR_START_RANGE - 10:
-      return 'NfsError::UnsuccessfulEncodeDecode';
-    case DNS_ERROR_START_RANGE:
-      return 'DnsError::DnsNameAlreadyRegistered';
-    case DNS_ERROR_START_RANGE - 1:
-      return 'DnsError::DnsRecordNotFound'
-    case DNS_ERROR_START_RANGE - 2:
-      return 'DnsError::ServiceAlreadyExists';
-    case DNS_ERROR_START_RANGE - 3:
-      return 'DnsError::ServiceNotFound';
-    case DNS_ERROR_START_RANGE - 4:
-      return 'DnsError::DnsConfigFileNotFoundOrCorrupted';
-    case DNS_ERROR_START_RANGE - 5:
-      return 'DnsError::UnexpectedError';
-    case DNS_ERROR_START_RANGE - 6:
-      return 'DnsError::UnsuccessfulEncodeDecode';
-    case FFI_ERROR_START_RANGE - 1:
-      return 'FfiError::PathNotFound';
-    case FFI_ERROR_START_RANGE - 2:
-      return 'InvalidPath';
-    case FFI_ERROR_START_RANGE - 3:
-      return 'PermissionDenied';
-    case FFI_ERROR_START_RANGE - 4:
-      return 'JsonParseError';
-    case FFI_ERROR_START_RANGE - 5:
-      return 'JsonDecodeError';
-    case FFI_ERROR_START_RANGE - 6:
-      return 'SpecificParseError';
-    case FFI_ERROR_START_RANGE - 7:
-      return 'JsonEncodeError';
-    case FFI_ERROR_START_RANGE - 8:
-      return 'LocalConfigAccessFailed';
-    case FFI_ERROR_START_RANGE - 9:
-      return 'Unexpected';
-    case FFI_ERROR_START_RANGE - 10:
-      return 'UnsuccessfulEncodeDecode';
-    default:
-      return 'Unexpected error';
-  }
+	let CLIENT_ERROR_START_RANGE = -1;
+	let NFS_ERROR_START_RANGE = CLIENT_ERROR_START_RANGE - 500;
+	let DNS_ERROR_START_RANGE = NFS_ERROR_START_RANGE - 500;
+	switch (errCode) {
+		case CLIENT_ERROR_START_RANGE:
+			return 'CoreError::StructuredDataHeaderSizeProhibitive';
+		case CLIENT_ERROR_START_RANGE - 1:
+			return 'CoreError::UnsuccessfulEncodeDecode';
+		case CLIENT_ERROR_START_RANGE - 2:
+			return 'CoreError::AsymmetricDecipherFailure';
+		case CLIENT_ERROR_START_RANGE - 3:
+			return 'CoreError::SymmetricDecipherFailure';
+		case CLIENT_ERROR_START_RANGE - 4:
+			return 'CoreError::ReceivedUnexpectedData';
+		case CLIENT_ERROR_START_RANGE - 5:
+			return 'CoreError::VersionCacheMiss';
+		case CLIENT_ERROR_START_RANGE - 6:
+			return 'CoreError::RootDirectoryAlreadyExists';
+		case CLIENT_ERROR_START_RANGE - 7:
+			return 'CoreError::RandomDataGenerationFailure';
+		case CLIENT_ERROR_START_RANGE - 8:
+			return 'CoreError::OperationForbiddenForClient';
+		case CLIENT_ERROR_START_RANGE - 9:
+			return 'CoreError::Unexpected';
+		case CLIENT_ERROR_START_RANGE - 10:
+			return 'CoreError::RoutingError';
+		case CLIENT_ERROR_START_RANGE - 11:
+			return 'CoreError::RoutingInterfaceError';
+		case CLIENT_ERROR_START_RANGE - 12:
+			return 'CoreError::UnsupportedSaltSizeForPwHash';
+		case CLIENT_ERROR_START_RANGE - 13:
+			return 'CoreError::UnsuccessfulPwHash';
+		case CLIENT_ERROR_START_RANGE - 14:
+			return 'CoreError::OperationAborted';
+		case CLIENT_ERROR_START_RANGE - 15:
+			return 'CoreError::MpidMessagingError';
+		case {CLIENT_ERROR_START_RANGE - 16:
+			return 'CoreError::GetFailure';
+		case {CLIENT_ERROR_START_RANGE - 17:
+			return 'CoreError::GetFailure';
+		case {CLIENT_ERROR_START_RANGE - 18:
+			return 'CoreError::GetFailure';
+		case {CLIENT_ERROR_START_RANGE - 19:
+			return 'CoreError::MutationFailure';
+		case {CLIENT_ERROR_START_RANGE - 20:
+			return 'CoreError::MutationFailure';
+		case {CLIENT_ERROR_START_RANGE - 21:
+			return 'CoreError::MutationFailure';
+		case {CLIENT_ERROR_START_RANGE - 22:
+			return 'CoreError::MutationFailure';
+		case {CLIENT_ERROR_START_RANGE - 23:
+			return 'CoreError::MutationFailure';
+		case {CLIENT_ERROR_START_RANGE - 24:
+			return 'CoreError::MutationFailure';
+		case {CLIENT_ERROR_START_RANGE - 25:
+			return 'CoreError::MutationFailure';
+		case {CLIENT_ERROR_START_RANGE - 26:
+			return 'CoreError::MutationFailure';
+		case {CLIENT_ERROR_START_RANGE - 27:
+			return 'CoreError::MutationFailure';
+		case CLIENT_ERROR_START_RANGE - 28:
+			return 'CoreError::SelfEncryption';
+		case NFS_ERROR_START_RANGE - 1:
+			return 'NfsError::DirectoryAlreadyExistsWithSameName';
+		case NFS_ERROR_START_RANGE - 2:
+			return 'NfsError::DestinationAndSourceAreSame';
+		case NFS_ERROR_START_RANGE - 3:
+			return 'NfsError::DirectoryNotFound';
+		case NFS_ERROR_START_RANGE - 4:
+			return 'NfsError::FileAlreadyExistsWithSameName';
+		case NFS_ERROR_START_RANGE - 5:
+			return 'NfsError::FileDoesNotMatch';
+		case NFS_ERROR_START_RANGE - 6:
+			return 'NfsError::FileNotFound';
+		case NFS_ERROR_START_RANGE - 7:
+			return 'NfsError::InvalidRangeSpecified';
+		case NFS_ERROR_START_RANGE - 8:
+			return 'NfsError::ParameterIsNotValid';
+		case NFS_ERROR_START_RANGE - 9:
+			return 'NfsError::Unexpected';
+		case NFS_ERROR_START_RANGE - 10:
+			return 'NfsError::UnsuccessfulEncodeDecode';
+		case NFS_ERROR_START_RANGE - 11:
+			return 'NfsError::SelfEncryption';
+		case DNS_ERROR_START_RANGE:
+			return 'DnsError::DnsNameAlreadyRegistered';
+		case DNS_ERROR_START_RANGE - 1:
+			return 'DnsError::DnsRecordNotFound';
+		case DNS_ERROR_START_RANGE - 2:
+			return 'DnsError::ServiceAlreadyExists';
+		case DNS_ERROR_START_RANGE - 3:
+			return 'DnsError::ServiceNotFound';
+		case DNS_ERROR_START_RANGE - 4:
+			return 'DnsError::DnsConfigFileNotFoundOrCorrupted';
+		case DNS_ERROR_START_RANGE - 5:
+			return 'DnsError::Unexpected';
+		case DNS_ERROR_START_RANGE - 6:
+			return 'DnsError::UnsuccessfulEncodeDecode';
+		default:
+			return 'Unexpected error';
+	}
 };
---
+++ tasks/grep_error_code.js
@@ -0,0 +1,127 @@
+'use strict';
+var gulp = require('gulp');
+var fs = require('fs');
+var pathUtil = require('path');
+var through = require('through2');
+var cp = require('child_process');
+
+var matchWords = {
+  'CORE': 'CLIENT_ERROR_START_RANGE',
+  'NFS': 'NFS_ERROR_START_RANGE',
+  'DNS': 'DNS_ERROR_START_RANGE'
+};
+
+var grepData = [];
+
+var destFile = './app/server/error_code_lookup.js';
+var template = 'export function errorCodeLookup(errCode) \{\n';
+
+// restyle the code
+var restyleFiles = function(fileArr, done) {
+  fs.readFile(fileArr[0].path, (err, data) => {
+    if (err) throw err;
+    var fileContent = data.toString();
+    fs.writeFile(fileArr[0].path, fileContent.replace(/=>\s*\{\s*/g, '=> {').replace(/\s*\}/g, '}'), (err, wData) => {
+      if (err) throw err;
+      fileArr.shift();
+      if (fileArr.length !== 0) {
+        restyleFiles(fileArr, done);
+      } else {
+        done();
+      }
+    });
+  });
+};
+
+// grep error code
+var grepFile = function(fileArr, done) {
+  var spawn = cp.spawn('grep', [ '-i', fileArr[0].word, fileArr[0].path ]);
+  var buf;
+  spawn.stdout.on('data', function(data) {
+    buf+=data;
+  });
+  spawn.on('exit', function() {
+    grepData.push(buf.toString());
+    fileArr.shift();
+    if (fileArr.length === 0) {
+      return done();
+    }
+    grepFile(fileArr, done);
+  })
+};
+
+// formate template
+var templateFile = function(grepContent) {
+  var parsedData = [];
+  for(let i = 0; i < grepContent.length; i++) {
+    var tempObj = {};
+    var dataArr = grepContent[i].split('\n');
+    tempObj.startVal = dataArr[0].slice(dataArr[0].indexOf('= ') + 2, -1);
+    tempObj.dataArr = dataArr;
+    parsedData.push(tempObj);
+  }
+  template += '\tlet ' + matchWords.CORE + ' = ' + parsedData[0].startVal + ';\n';
+  template += '\tlet ' + matchWords.NFS + ' = ' + parsedData[1].startVal + ';\n';
+  template += '\tlet ' + matchWords.DNS + ' = ' + parsedData[2].startVal + ';\n';
+  template += '\tswitch (errCode) {\n';
+
+  var grepErrorArr = function(target) {
+    for(let err in target) {
+      if (err !== '0') {
+        var caseIndex = target[err].indexOf('=> ');
+        var caseIndexEnd = -1;
+        if (caseIndex !== -1) {
+          var caseStr = target[err].slice(caseIndex + 3, caseIndexEnd);
+          var statement = target[err].slice(0, caseIndex);
+          if (statement.indexOf('(_)') !== -1) {
+            statement = statement.replace(/\(_\)/g, '');
+          }
+          caseStr = caseStr.replace(/,\}\}/g, '');
+          statement = statement.replace(/\{\D*\}/, '');
+          template += '\t\tcase ' + caseStr.trim() + ':\n';
+          template += '\t\t\treturn \'' + statement.trim() + '\';\n'
+        }
+      }
+    }
+  };
+  for(let i = 0; i < parsedData.length; i++) {
+    grepErrorArr(parsedData[i].dataArr);
+  }
+  template += '\t\tdefault:\n\t\t\treturn \'Unexpected error\';\n\t}\n};';
+  fs.writeFile(pathUtil.resolve('.', destFile), template, (err, data) => {
+    if (err) throw err;
+    console.log(destFile + ' updated.');
+  });
+};
+
+var grepStream = through({ objectMode: true }, function(srcFile, enc, cb) {
+  var files = [
+    {
+      path: pathUtil.resolve(srcFile.path, 'core', 'errors.rs'),
+      word: matchWords.CORE
+    },
+    {
+      path: pathUtil.resolve(srcFile.path, 'nfs', 'errors.rs'),
+      word: matchWords.NFS
+    },
+    {
+      path: pathUtil.resolve(srcFile.path, 'dns', 'errors.rs'),
+      word: matchWords.DNS
+    }
+  ];
+
+  var afterRestyleFiles = () => {
+    grepFile(files.slice(), function() {
+      console.log('Finished grep');
+      templateFile(grepData);
+    });
+  };
+  restyleFiles(files.slice(), afterRestyleFiles);
+  this.push(srcFile.path);
+  cb();
+});
+
+gulp.task('grep_error_code', function() {
+  gulp.src('./safe_core/src', { read: false })
+  .pipe(grepStream);
+});
